5052071465574252334e316fc0569b4182473d2b
javajigi <parkjoohan0603@gmail.com>
2021-04-16 15:59:02 +0900

    feat: use assembly language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,41 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+	@screen
+	M=0
+(LOOP)
+	@24576
+	D=M
+	@DRAW
+	D;JNE
+	@CLEAN
+	D;JEQ
+(DRAW)
+	@screen
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1
+	@8192
+	D=D-A
+	@PLUS
+	D;JLT
+	@LOOP
+	0;JMP
+(PLUS)
+	@screen
+	M=M+1
+	@LOOP
+	0;JMP
+(CLEAN)
+	@screen
+	D=M
+	@SCREEN
+	A=A+D
+	M=0
+	@MINUS
+	D;JGT
+	@LOOP
+	0;JMP
+(MINUS)
+	@screen
+	M=M-1
+	@LOOP
+	0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,25 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+	@2
+	M=0
+	@time
+	M=0
+(LOOP)
+	@time
+	D=M
+	@1
+	D=D-M
+	@END
+	D;JEQ
+	@0
+	D=M
+	@2
+	M=M+D
+	@time
+	M=M+1
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
